560730296e313723fc19689252d289c7e260f288
mounika <saimounikapasupuleti333@gmail.com>
2023-05-13 11:42:53 +0530

    varaibles ex source code

---
+++ src/basics/VariablesEx.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class VariablesEx {
+
+	public static void main(String args[])
+	{
+		int ticketPrice;
+		ticketPrice=9000;
+		System.out.println(ticketPrice);
+		System.out.println("The price of the ticket is"+9000);
+	}
+}
