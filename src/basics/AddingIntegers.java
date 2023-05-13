6fde5dd50ac9c32a14bae9ca5893402eac69868b
mounika <saimounikapasupuleti333@gmail.com>
2023-05-13 11:57:10 +0530

    adding integers source code

---
+++ src/basics/AddingIntegers.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class AddingIntegers {
+
+	public static void main(String args[])
+	{
+	int number1;
+	int number2;
+	number1= 14;
+	number2=12;
+	System.out.println(number1+number2);
+	}
+}
